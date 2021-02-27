3e16d1be786f1b22c113c8a43e4e69e72d9fe802
jp-signum <patrick@jpsignum.com>
2021-02-27 02:45:39 +0000

    created initial partial test file modeled after _form_question

---
+++ spec/views/quizzes/quizzes/_copy_question.html.erb_spec.rb
@@ -0,0 +1,15 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../../views_helper')
+
+# template
+# describe "/quizzes/quizzes/_form_question" do
+
+#   it "should render" do
+#     course_with_student
+#     view_context
+#     assign(:js_env, {quiz_max_combination_count: 200})
+#     render :partial => "quizzes/quizzes/form_question"
+
+#     expect(response).not_to be_nil
+#   end
+# end
